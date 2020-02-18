06d5e055eca92fe6641a5d16729571c6a42f6215
Suku <ilzammulkhaq85@gmail.com>
2020-02-18 17:48:23 +0700

    Building Base Api Exceptions

---
+++ lib/service/api_exceptions.dart
@@ -0,0 +1,9 @@
+/// [BussinessException] Is Some Mechanism To Handling Error Message From Your API
+class BussinessException {
+  
+}
+
+/// [UnknownException] Is Some Mechanism To Handling Your Unknown Exception
+class UnknownException {
+  
+}
