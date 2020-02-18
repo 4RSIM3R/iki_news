04efc5c11a252af7d0fe0ee437a4f9de0250fc15
Suku <ilzammulkhaq85@gmail.com>
2020-02-18 17:48:10 +0700

    Building Base News Repository

---
+++ lib/repository/news_repository.dart
@@ -0,0 +1,10 @@
+import 'package:iki_news/model/api_service_model.dart';
+import 'package:iki_news/model/news_api_response.dart';
+
+class NewsRepository {
+  
+  Future<ApiServiceModel<NewsApiResponse>> getAmericanData() async {
+    // TODO : Call Your Apibase Here To Get Some Data
+  }
+
+}
